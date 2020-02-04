c7fd2c404bf626f9fa5c2899b02d7d1148a640c0
Your Name <you@example.com>
2020-02-04 09:50:21 -0800

    mostly working need to work out case issues

--- dicts-word-count/wordcount.py
+++ dicts-word-count/wordcount.py
@@ -1,20 +1,24 @@
 def word_counter(file_name):
     """"count the # of times a word is in a txt"""
     words = open(file_name)
+    #open file and give a veriable
     dictionary = {}
+    #creating an empty dictionary to hold word from words 
     for line in words:
+        #creating a loop to itirate over words line by line
         line = line.rstrip()
+        #stripping the current line of blank space on the right side
         split_line = line.split(" ")
+        #returing a list of strings broken up by one space
         for word in split_line:
-            if "." in word:
+            #creating a loop that itirates over split_line list item by item
+            if ("." in word or "," in word or "?" in word):
+                #creating a conditional statement to run more code if the condition is met
                 word = word[:-1]
-                print(word)
-    print()            
-   
+                #removing the last character from word
+            dictionary[word] = dictionary.get(word, 0) + 1
+                #assigning dictionary value of word 
+    return dictionary            
 
-
-
-
-
-word_counter("test.txt")
-#.rstrip(,)
+         
+print(word_counter("test.txt"))
